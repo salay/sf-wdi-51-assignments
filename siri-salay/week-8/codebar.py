c3568431030a48043c6dda3b431d4f5fc5f48e06
salay <siri.alay1@gmail.com>
2019-03-13 09:15:16 -0700

    finished codebar refactor

--- siri-salay/week-8/codebar.py
+++ siri-salay/week-8/codebar.py
@@ -1,51 +1,60 @@
 
 class Member():
-    def __init__(self, name='', isStudent = bool, reason='', bio=''):
-        self.fullName = name
-        self.isStudent = isStudent
+    def __init__(self, name=''):
+        self.full_name = name
+    def say_hello(self):
+        print('Hello, I\'m', self.full_name)
+    
+class Student(Member):
+    def __init__(self, name, reason):
+        Member.__init__(self)
+        self.full_name = name
         self.reason = reason
+
+class Instructor(Member):
+    def __init__(self, name, bio):
+        Member.__init__(self)
+        self.full_name = name
         self.bio = bio
         self.skills = []
-    def say_hello(self):
-        print('Hello, I\'m', self.fullName)
     def add_skill(self, skill):
-        if self.isStudent == False:
-            self.skills.append(skill)
-        else:
-            print("cannot add skill to", self.fullName, "because this member is not an instructor")
-
-
+        self.skills.append(skill)
+       
 class Workshop():
     def __init__(self, date ='', subject=''):
         self.date = date
         self.subject = subject
         self.instructors = []
         self.students = []
-    def add_participant(self, Member):
-        if Member.isStudent == True:
-            self.students.append(Member.fullName)
+    def add_participant(self, participant):
+        if isinstance(participant, Student) == True:
+            self.students.append(participant)
         else:
-            self.instructors.append(Member.fullName)
+            self.instructors.append(participant)
+    def print_details(self):
+        print("the date of the workshop is", self.date)
+        print("the subject of the workshop is", self.subject)
+        print("the students in the workshop are: ")
+        for i in range(len(self.students)):
+            print(self.students[i].full_name, " : ", self.students[i].reason)
+        print("the instructors in the workshop are: ")
+        for i in range(len(self.instructors)):
+            print(self.instructors[i].full_name, "whose skills are: ", ", ".join(self.instructors[i].skills))
+            print("     ", self.instructors[i].bio)
 
+workshop = Workshop("12/03/2014", "Shutl")
 
-jane = Member("Jane Doe", True, "I am trying to learn programming and need some help")
-lena = Member("Lena Smith", True,  "I am really excited about learning to program!")
-vicky = Member("Vicky Python", False, "I want to help people learn coding.", "I am actually a reptile.")
-
+jane = Student("Jane Doe", "I am trying to learn programming and need some help")
+lena = Student("Lena Smith", "I am really excited about learning to program!")
+vicky = Instructor("Vicky Python", "I want to help people learn coding.")
 vicky.add_skill("HTML")
 vicky.add_skill("JavaScript")
-jane.add_skill("things")
+nicole = Instructor("Nicole McMillan", "I have been programming for 5 years in Python and want to spread the love")
+nicole.add_skill("Python")
 
-print(lena.skills)
-print(vicky.skills)
-print(vicky.bio)
-print(lena.bio)
+workshop.add_participant(jane)
+workshop.add_participant(lena)
+workshop.add_participant(vicky)
+workshop.add_participant(nicole)
 
-workshop1 = Workshop("12/03/2014", "Shutl")
-
-workshop1.add_participant(jane)
-workshop1.add_participant(lena)
-workshop1.add_participant(vicky)
-
-print(workshop1.instructors)
-print(workshop1.students)
+workshop.print_details()
